922111a75675ae742a9daeea6090b16ec86ab674
abelzis <abelzis@hotmail.com>
2019-09-19 23:40:17 +0300

    Version v0.1a-1 - Updated UI with messy code

--- Praktikos Darbas 1 - HashApp/Praktikos Darbas 1 - HashApp.cpp
+++ Praktikos Darbas 1 - HashApp/Praktikos Darbas 1 - HashApp.cpp
@@ -1,9 +1,44 @@
 // Praktikos Darbas 1 - HashApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include "hash.h"
+
+//read from file and write to file
+void output(std::ifstream& inp, Hash hash)
+{
+	string text;
+	while (!inp.eof())// && !text.empty())
+	{
+		//read text
+		string temp;
+		inp >> temp;
+		text += temp;
+	}
+
+	if (text.empty())
+	{
+		std::cout << "\nFile is empty. Please retry.\n";
+		inp.close();
+		return;
+	}
+
+	if (!text.empty())
+	{
+		std::cout << "\n\nText: " << text;
+		hash.setHash(text);
+
+		std::ofstream out("hash.txt");
+		
+		out << hash.getHash();
+		std::cout << "\nHash: " << hash.getHash() << "\n";
+
+		return;
+	}
+}
+
 
 int main()
 {
@@ -17,35 +52,80 @@ int main()
 	//in file
 	if (inp.is_open())
 	{
-		while (!inp.eof())
-		{
-			//read text
-			string temp;
-			inp >> temp;
-			text += temp;
-		}
+		std::cout << "Default text file \"data.txt\" is opened. Trying to read file...\n";
 
-		if (text.empty())
-			inp.close();
-		if (!text.empty())
-		{
-			hash.setHash(text);
-
-			std::ofstream out("hash.txt");
-			out << hash.getHash();
-		}
+		output(inp, hash);
 	}
+
+
 	//by hand in console
 	if (!inp.is_open())
 	{
-		std::cout << "Please input text to hash: ";
-		std::getline(std::cin, text);
-		while (text.empty())
-			std::getline(std::cin, text);
+		//UI
+		while (1)
+		{
+			std::cout << "No file is opened. Open file? (Y/N): ";
+			char condition_char;
+			std::cin >> condition_char;
 
-		hash.setHash(text);
+			// if accept, read from file
+			if (condition_char == 'Y' || condition_char == 'y')
+			{
+				std::cout << "\nEnter file name (filename.txt): ";
+				string filename;
+				std::cin >> filename;
 
-		std::cout << hash.getHash() << "\n";
+				//open file
+				try 
+				{
+					inp.open(filename, std::fstream::in);
+					//if open
+					if (inp.is_open())
+					{
+						output(inp, hash);	//extract text and hash
+						if (!inp.is_open())	//if not opened
+							continue;
+						break;
+					}
+					else
+					{
+						std::cout << "\nFile could not be opened. Please check the file name.\n";
+						continue;
+					}
+				}
+				catch (std::exception ex)
+				{
+					std::cout << "\nError: " << ex.what();
+				}
+
+
+			}
+			// if not accept, insert string in console
+			else if (condition_char == 'N' || condition_char == 'n')
+			{
+				try
+				{
+					//get text
+					std::cout << "Please input text to hash: ";
+					std::getline(std::cin, text);
+					while (text.empty())
+						std::getline(std::cin, text);
+					if (!text.empty())
+						break;
+
+					hash.setHash(text);
+
+					std::cout << "Hash: " << hash.getHash() << "\n";
+				}
+				catch (std::exception ex)
+				{
+					std::cout << "\nError: " << ex.what();
+					continue;
+				}
+			}
+			else
+				std::cout << "\nInvalid symbol. ";
+		}
 	}
 
 
